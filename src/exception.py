df7af9b8beb30cc5dd7746070210aeb78efd5dbc
shubhamprajapati241 <shubhamprajapati241@gmail.com>
2024-11-10 18:37:23 +0530

    added-notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 import sys 
+from src.logger import logging
 
-class customexception(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_details: sys):
         self.error_message = error_message
         _,_, exec_tb = error_details.exc_info()
@@ -9,3 +10,10 @@ class customexception(Exception):
 
     def __str__(self):
         return "Error occured in Python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name, self.line_no, str(self.error_message))
+
+if __name__ == "__main__":
+    try :
+        a = 1/0
+    except Exception as e:
+        logging.error("Division by zero")
+        raise CustomException(e, sys)
